d6ec9f7c4aed603b210a6e5d18d58f636bc85b28
digantaduworah <digantaduworah@gmail.com>
2026-04-24 15:10:46 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -15,3 +15,33 @@ firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
 console.log("Firebase Connected");
+function submitOrder() {
+  try {
+    let phone = document.getElementById("phone").value;
+    let service = document.getElementById("service").value;
+    let amount = document.getElementById("total").innerText;
+
+    if (!phone || !service) {
+      alert("Fill all fields");
+      return;
+    }
+
+    let orderId = "CSC" + Date.now();
+
+    db.collection("orders").add({
+      phone,
+      service,
+      amount,
+      orderId,
+      status: "Pending",
+      time: new Date().toString()
+    }).then(() => {
+      alert("Order Submitted Successfully!");
+      document.getElementById("qrBox").style.display = "none";
+    });
+
+  } catch (e) {
+    console.log(e);
+    alert("System Error");
+  }
+}
