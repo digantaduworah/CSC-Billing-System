231f04acd524918ad629088f69b05e513e6fbca4
digantaduworah <digantaduworah@gmail.com>
2026-04-24 11:58:10 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBjJSoGkYlDjbPA0lqF-4yu9UWw6SpHTSQ",
   authDomain: "duarah-store.firebaseapp.com",
   projectId: "duarah-store",
-  storageBucket: "duarah-store.firebasestorage.app",
+  storageBucket: "duarah-store.appspot.com"
   messagingSenderId: "67666768092",
   appId: "1:67666768092:web:2cbc90004c8a2a0aac4419",
   measurementId: "G-EV5KNZ22G5"
@@ -11,6 +11,9 @@ const firebaseConfig = {
 
 // ✅ Correct initialization (compat version)
 firebase.initializeApp(firebaseConfig);
+db.settings({
+  ignoreUndefinedProperties: true
+});
 
 // ✅ Services
 const db = firebase.firestore();
